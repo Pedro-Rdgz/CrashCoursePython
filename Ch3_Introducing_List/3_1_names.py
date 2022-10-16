2b283d042801de63e134ba0000e0262ae09bb329
Pedro Rodriguez <pedroRdgz@proton.me>
2022-10-15 20:07:31 -0700

    Modifications to files from Chapter three

--- Ch3_Introducing_List/3_1_names.py
+++ Ch3_Introducing_List/3_1_names.py
@@ -4,7 +4,7 @@ Print each person's name by accessing each element
 in the list, one at a time.
 """
 
-names = ['pedro', 'andrea', 'rodo', 'kike']
+names = ['megaman', 'roll', 'bass', 'proto']
 print(names[0].title())
 print(names[1].title())
 print(names[2].title())
--- Ch3_Introducing_List/3_2_greetings.py
+++ Ch3_Introducing_List/3_2_greetings.py
@@ -4,7 +4,7 @@ printing each person's name, print a message to them. The text of
 each message should be the same, but each message should be 
 personalized with the person's name.
 """
-names = ['pedro', 'andrea', 'rodo', 'kike']
+names = ['megaman', 'roll', 'bass', 'proto']
 greeting = "Hello "
 greeting2 = ", have a nice day!"
 print(greeting + names[0].title() + greeting2)
